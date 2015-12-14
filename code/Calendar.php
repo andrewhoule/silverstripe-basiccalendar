336b0204ceac504df50d429ef0087880caf27e25
Peter Thaleikis <petert@catalyst.net.nz>
2015-12-15 02:03:54 +1300

    convert to PSR-2

--- code/Calendar.php
+++ code/Calendar.php
@@ -1,38 +1,41 @@
-<?php 
- 
-class Calendar extends Page {
+<?php
 
-  private static $db = array(
+
+class Calendar extends Page
+{
+    private static $db = array(
     'EventsPerPage' => 'Int',
     'PhotoThumbnailHeight' => 'Int',
     'PhotoThumbnailWidth' => 'Int',
     'PhotoFullHeight' => 'Int',
     'PhotoFullWidth' => 'Int',
-    'ShowExcerpt' => 'Boolean'
+    'ShowExcerpt' => 'Boolean',
   );
 
-  private static $has_many = array(
-    'CalendarEntries' => 'CalendarEntry'
+    private static $has_many = array(
+    'CalendarEntries' => 'CalendarEntry',
   );
 
-  public function singular_name() {
-    return "Event Listing Page";
-  }
+    public function singular_name()
+    {
+        return 'Event Listing Page';
+    }
 
-  private static $defaults = array(
+    private static $defaults = array(
     'EventsPerPage' => '20',
     'PhotoThumbnailWidth' => '150',
     'PhotoThumbnailHeight' => '150',
     'PhotoFullWidth' => '700',
     'PhotoFullHeight' => '700',
-    'ShowExcerpt' => true
+    'ShowExcerpt' => true,
   );
 
-  private static $icon = "basiccalendar/images/calendar";
+    private static $icon = 'basiccalendar/images/calendar';
 
-  public function getCMSFields() {
-    $fields = parent::getCMSFields();
-    $CalendarGridField = new GridField(
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $CalendarGridField = new GridField(
       'CalendarEntries',
       'Events',
       $this->CalendarEntries(),
@@ -46,74 +49,80 @@ class Calendar extends Page {
         ->addComponent(new GridFieldDeleteAction())
         ->addComponent(new GridFieldDetailForm())
       );
-    $fields->addFieldToTab("Root.Events", $CalendarGridField);
-    $fields->addFieldToTab("Root.Config", CheckboxField::create("ShowExcerpt")->setTitle("Show Excerpts on Holder Page"));
-    $fields->addFieldToTab("Root.Config", SliderField::create("EventsPerPage","Number of Events Per Page",1,50));
-    $fields->addFieldToTab("Root.Config", SliderField::create("PhotoThumbnailWidth","Photo Thumbnail Width",50,400));
-    $fields->addFieldToTab("Root.Config", SliderField::create("PhotoThumbnailHeight","Photo Thumbnail Height",50,400));
-    $fields->addFieldToTab("Root.Config", SliderField::create("PhotoFullWidth","Photo Fullsize Width",400,1200));
-    $fields->addFieldToTab("Root.Config", SliderField::create("PhotoFullHeight","Photo Fullsize Height",400,1200));
-    return $fields;
-  }
+        $fields->addFieldToTab('Root.Events', $CalendarGridField);
+        $fields->addFieldToTab('Root.Config', CheckboxField::create('ShowExcerpt')->setTitle('Show Excerpts on Holder Page'));
+        $fields->addFieldToTab('Root.Config', SliderField::create('EventsPerPage', 'Number of Events Per Page', 1, 50));
+        $fields->addFieldToTab('Root.Config', SliderField::create('PhotoThumbnailWidth', 'Photo Thumbnail Width', 50, 400));
+        $fields->addFieldToTab('Root.Config', SliderField::create('PhotoThumbnailHeight', 'Photo Thumbnail Height', 50, 400));
+        $fields->addFieldToTab('Root.Config', SliderField::create('PhotoFullWidth', 'Photo Fullsize Width', 400, 1200));
+        $fields->addFieldToTab('Root.Config', SliderField::create('PhotoFullHeight', 'Photo Fullsize Height', 400, 1200));
 
-  public function getUpcomingEvents() {
-    $calendarentries = $this->getComponents("CalendarEntries")->sort("StartDate","ASC");
-    $calendarentrylist = new ArrayList();
-    if($calendarentries) {
-      foreach($calendarentries AS $calendarentry) {
-        if($calendarentry->EndDate) {
-          if(strtotime($calendarentry->EndDate) > strtotime("yesterday")) {
-            $calendarentrylist->push($calendarentry);
-          }
-        }
-        elseif(strtotime($calendarentry->StartDate) > strtotime("yesterday")) {
-          $calendarentrylist->push($calendarentry);
-        }
-      }
-      return $calendarentrylist;
+        return $fields;
     }
-  }
 
-  public function UpcomingEvents($limit=5) {
-    return $this->getUpcomingEvents()->limit($limit);
-  }
- 
+    public function getUpcomingEvents()
+    {
+        $calendarentries = $this->getComponents('CalendarEntries')->sort('StartDate', 'ASC');
+        $calendarentrylist = new ArrayList();
+        if ($calendarentries) {
+            foreach ($calendarentries as $calendarentry) {
+                if ($calendarentry->EndDate) {
+                    if (strtotime($calendarentry->EndDate) > strtotime('yesterday')) {
+                        $calendarentrylist->push($calendarentry);
+                    }
+                } elseif (strtotime($calendarentry->StartDate) > strtotime('yesterday')) {
+                    $calendarentrylist->push($calendarentry);
+                }
+            }
+
+            return $calendarentrylist;
+        }
+    }
+
+    public function UpcomingEvents($limit = 5)
+    {
+        return $this->getUpcomingEvents()->limit($limit);
+    }
 }
- 
-class Calendar_Controller extends Page_Controller {
 
-  private static $allowed_actions = array(
-    'show'
+class Calendar_Controller extends Page_Controller
+{
+    private static $allowed_actions = array(
+    'show',
   );
 
-  public function getCalendarEntry() {
-    $Params = $this->getURLParams();
-    if(is_numeric($Params['ID']) && $CalendarEntry = CalendarEntry::get()->byID((int)$Params['ID'])) {
-      return $CalendarEntry;
+    public function getCalendarEntry()
+    {
+        $Params = $this->getURLParams();
+        if (is_numeric($Params['ID']) && $CalendarEntry = CalendarEntry::get()->byID((int) $Params['ID'])) {
+            return $CalendarEntry;
+        }
     }
-  }
 
-  public function show() {       
-    if($CalendarEntry = $this->getCalendarEntry()) {
-      $Data = array(
-        'CalendarEntry' => $CalendarEntry
+    public function show()
+    {
+        if ($CalendarEntry = $this->getCalendarEntry()) {
+            $Data = array(
+        'CalendarEntry' => $CalendarEntry,
       );
-      return $this->Customise($Data);
-    }
-    else {
-      return $this->httpError(404, 'Sorry that calendar entry could not be found');
-    }
-  }
 
-  public function init() {
-    parent::init();
-      Requirements::CSS("basiccalendar/css/calendar.css");
-  }
+            return $this->Customise($Data);
+        } else {
+            return $this->httpError(404, 'Sorry that calendar entry could not be found');
+        }
+    }
 
-  public function PaginatedUpcomingEvents() {
-    $PaginatedUpcomingEvents = new PaginatedList($this->getUpcomingEvents(), $this->request);
-    $PaginatedUpcomingEvents->setPageLength($this->EventsPerPage ? $this->EventsPerPage : '20');
-    return $PaginatedUpcomingEvents;
-  }
-  
+    public function init()
+    {
+        parent::init();
+        Requirements::CSS('basiccalendar/css/calendar.css');
+    }
+
+    public function PaginatedUpcomingEvents()
+    {
+        $PaginatedUpcomingEvents = new PaginatedList($this->getUpcomingEvents(), $this->request);
+        $PaginatedUpcomingEvents->setPageLength($this->EventsPerPage ? $this->EventsPerPage : '20');
+
+        return $PaginatedUpcomingEvents;
+    }
 }
--- code/CalendarEntry.php
+++ code/CalendarEntry.php
@@ -1,53 +1,68 @@
 <?php
 
-class CalendarEntry extends DataObject {
-
-  private static $db = array(
+class CalendarEntry extends DataObject
+{
+    private static $db = array(
     'StartDate' => 'Date',
     'EndDate' => 'Date',
     'StartTime' => 'Time',
     'EndTime' => 'Time',
     'Title' => 'Varchar(255)',
     'Location' => 'Text',
-    'Content' => 'HTMLText'
+    'Content' => 'HTMLText',
   );
 
-  private static $has_one = array(
+    private static $has_one = array(
     'Resource' => 'File',
     'Photo' => 'Image',
-    'Calendar' => 'Calendar'
+    'Calendar' => 'Calendar',
   );
 
-  private static $summary_fields = array( 
+    private static $summary_fields = array(
     'Thumbnail' => 'Photo',
     'NiceDates' => 'Date',
     'Title' => 'Title',
-    'ContentExcerpt' => 'Content'
+    'ContentExcerpt' => 'Content',
   );
 
-  public function singular_name() {
-    return "Event";
-  }
+    public function singular_name()
+    {
+        return 'Event';
+    }
 
-  public function canCreate($Member = null) { return true; }
-  public function canEdit($Member = null) { return true; }
-  public function canView($Member = null) { return true; }
-  public function canDelete($Member = null) { return true; }
+    public function canCreate($Member = null)
+    {
+        return true;
+    }
+    public function canEdit($Member = null)
+    {
+        return true;
+    }
+    public function canView($Member = null)
+    {
+        return true;
+    }
+    public function canDelete($Member = null)
+    {
+        return true;
+    }
 
-  public function getCMSFields() {
-    $startdatefield = new DateField('StartDate','Start Date');
-    $startdatefield->setConfig('showcalendar', true);
-    $startdatefield->setConfig('dateformat','MM/dd/yyyy'); 
-    $enddatefield = new DateField('EndDate','End Date');
-    $enddatefield->setConfig('showcalendar', true);
-    $enddatefield->setConfig('dateformat','MM/dd/yyyy');
-    $imagefield = UploadField::create('Photo');
-    $imagefield->folderName = "Calendar"; 
-    $imagefield->getValidator()->allowedExtensions = array('jpg','jpeg','gif','png');
-    $resourcefield = UploadField::create("Resource")->setTitle("Flyer/Brochure (PDF or Doc)");
-    $resourcefield->folderName = "Calendar"; 
-    $resourcefield->getValidator()->allowedExtensions = array("pdf","doc","docx");
-    return new FieldList(
+    public function getCMSFields()
+    {
+        $startdatefield = new DateField('StartDate', 'Start Date');
+        $startdatefield->setConfig('showcalendar', true);
+        $startdatefield->setConfig('dateformat', 'MM/dd/yyyy');
+        $enddatefield = new DateField('EndDate', 'End Date');
+        $enddatefield->setConfig('showcalendar', true);
+        $enddatefield->setConfig('dateformat', 'MM/dd/yyyy');
+        $imagefield = UploadField::create('Photo');
+        $imagefield->folderName = 'Calendar';
+        $imagefield->getValidator()->allowedExtensions = array('jpg', 'jpeg', 'gif', 'png');
+        $resourcefield = UploadField::create('Resource')->setTitle('Flyer/Brochure (PDF or Doc)');
+        $resourcefield->folderName = 'Calendar';
+        $resourcefield->getValidator()->allowedExtensions = array('pdf', 'doc', 'docx');
+
+        return new FieldList(
       TextField::create('Title'),
       $startdatefield,
       $enddatefield,
@@ -58,88 +73,110 @@ class CalendarEntry extends DataObject {
       $resourcefield,
       HTMLEditorField::create('Content')->setTitle('Event Description')
     );
-  }
-
-  public function Link() {
-    return $this->Calendar()->Link("show")."/".$this->ID;
-  }
-
-  public function ContentExcerpt($length = 200) {
-    $text = strip_tags($this->Content);
-    $length = abs((int)$length);
-    if(strlen($text) > $length) {
-      $text = preg_replace("/^(.{1,$length})(\s.*|$)/s", '\\1...', $text);
     }
-    return $text;
-  }
 
-  public function NiceStartDate() {
-    return $this->obj('StartDate')->Format("F j, Y");
-  }
-    
-  public function NiceEndDate() {
-    return $this->obj('EndDate')->Format("F j, Y");
-  }
+    public function Link()
+    {
+        return $this->Calendar()->Link('show').'/'.$this->ID;
+    }
 
-  public function NiceDates() {
-    if($this->StartDate AND $this->EndDate)
-      if($this->StartDate == $this->EndDate)
-        return $this->NiceStartDate();
-      else
-        return $this->NiceStartDate() . " – " . $this->NiceEndDate();
-    elseif($this->StartDate)
-      return $this->NiceStartDate();
-    else
-      return false;
-  }
+    public function ContentExcerpt($length = 200)
+    {
+        $text = strip_tags($this->Content);
+        $length = abs((int) $length);
+        if (strlen($text) > $length) {
+            $text = preg_replace("/^(.{1,$length})(\s.*|$)/s", '\\1...', $text);
+        }
 
-  public function NiceStartTime() {
-    return $this->obj('StartTime')->Nice(); 
-  }
+        return $text;
+    }
 
-  public function NiceEndTime() {
-    return $this->obj('EndTime')->Nice(); 
-  }
+    public function NiceStartDate()
+    {
+        return $this->obj('StartDate')->Format('F j, Y');
+    }
 
-  public function NiceTimes() {
-    if($this->StartTime AND $this->EndTime)
-      return $this->NiceStartTime() . " – " . $this->NiceEndTime();
-    elseif($this->StartTime)
-      return $this->NiceStartTime();
-    else 
-      return false;
-  }
+    public function NiceEndDate()
+    {
+        return $this->obj('EndDate')->Format('F j, Y');
+    }
 
-  public function CalendarLink() {
-    return $this->getComponent('Calendar')->Link();
-  }
+    public function NiceDates()
+    {
+        if ($this->StartDate and $this->EndDate) {
+            if ($this->StartDate == $this->EndDate) {
+                return $this->NiceStartDate();
+            } else {
+                return $this->NiceStartDate().' – '.$this->NiceEndDate();
+            }
+        } elseif ($this->StartDate) {
+            return $this->NiceStartDate();
+        } else {
+            return false;
+        }
+    }
 
-  public function PhotoCropped($x=120,$y=120) {
-    $width = $this->Calendar()->PhotoThumbnailWidth;
-    $height = $this->Calendar()->PhotoThumbnailHeight;
-    if($width != 0) 
-      $x = $width;
-    if($height != 0) 
-      $y = $height;
-    return $this->Photo()->CroppedImage($x,$y);
-  }
-  
-  public function PhotoSized($x=700,$y=700) {
-    $width = $this->Calendar()->PhotoFullWidth;
-    $height = $this->Calendar()->PhotoFullHeight;
-    if($width != 0) 
-      $x = $width;
-    if($height != 0) 
-      $y = $height;
-    return $this->Photo()->SetRatioSize($x,$y);
-  }
+    public function NiceStartTime()
+    {
+        return $this->obj('StartTime')->Nice();
+    }
 
-  public function Thumbnail() {
-    $Image = $this->Photo();
-    if ($Image) 
-      return $Image->CMSThumbnail();
-    else 
-      return null;
-  }
+    public function NiceEndTime()
+    {
+        return $this->obj('EndTime')->Nice();
+    }
 
+    public function NiceTimes()
+    {
+        if ($this->StartTime and $this->EndTime) {
+            return $this->NiceStartTime().' – '.$this->NiceEndTime();
+        } elseif ($this->StartTime) {
+            return $this->NiceStartTime();
+        } else {
+            return false;
+        }
+    }
+
+    public function CalendarLink()
+    {
+        return $this->getComponent('Calendar')->Link();
+    }
+
+    public function PhotoCropped($x = 120, $y = 120)
+    {
+        $width = $this->Calendar()->PhotoThumbnailWidth;
+        $height = $this->Calendar()->PhotoThumbnailHeight;
+        if ($width != 0) {
+            $x = $width;
+        }
+        if ($height != 0) {
+            $y = $height;
+        }
+
+        return $this->Photo()->CroppedImage($x, $y);
+    }
+
+    public function PhotoSized($x = 700, $y = 700)
+    {
+        $width = $this->Calendar()->PhotoFullWidth;
+        $height = $this->Calendar()->PhotoFullHeight;
+        if ($width != 0) {
+            $x = $width;
+        }
+        if ($height != 0) {
+            $y = $height;
+        }
+
+        return $this->Photo()->SetRatioSize($x, $y);
+    }
+
+    public function Thumbnail()
+    {
+        $Image = $this->Photo();
+        if ($Image) {
+            return $Image->CMSThumbnail();
+        } else {
+            return;
+        }
+    }
 }
